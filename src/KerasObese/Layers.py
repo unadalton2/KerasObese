f9a5dcf2b3234aab1cb9608d20360a9e62f2e0c9
ornateByte <null@gmail.com>
2022-06-02 13:56:58 -0600

    Added correct Errors to DenseLayer.__init__()

--- src/KerasObese/Layers.py
+++ src/KerasObese/Layers.py
@@ -26,44 +26,43 @@ class Layer:
 
 
 class DenseLayer(Layer):
-    def __init__(self, layer: Dense = None, Weights: list = None):
+    def __init__(self, layer: Dense, Weights: list = None):
         if not isinstance(layer, Dense):
-            raise TypeError("layer must be a dense layer, instead got "+type(layer).__name__)
-        if not isinstance(layer, type(None)):
-            super().__init__(layer)
-            self.Weights = layer.get_weights()
-        else:
-            if not isinstance(Weights, type(None)):
-                # TODO create Dense from weights
-                super().__init__(Dense())
-            else:
-                raise RuntimeError  # TODO add Correct Error
+            raise TypeError(
+                "layer must be a dense layer, instead got "+type(layer).__name__)
+        super().__init__(layer)
+        self.Weights = layer.get_weights()
+
         if not isinstance(Weights, type(None)):
+            if not isinstance(Weights, list):
+                raise TypeError(
+                    "Weights must be a list of numpy arrays instead got: "+type(Weights).__name__)
+            if len(Weights) != 2:
+                raise ValueError(
+                    "Weights must be a list with length 2 instead got: " + len(Weights))
+            for i in range(len(Weights)):
+                if not isinstance(Weights[i], np.ndarray):
+                    raise TypeError("Weights must be a list of only numpy arrays found: " +
+                                    type(Weights[i]).__name__+" at position: "+str(i))
             self.Weights = Weights
 
         self.activation = self.layer.activation
-    """
-    def __init__(self, Type: str, Weights: np.array):
-        raise NotImplementedError
-        super().__init__(layer)
-        # TODO Add type Checking
-        self.Weights = layer.get_weights()
-    """
 
     def setWeights(self, Weights):
-        # TODO Add type Checking
+
         if not isinstance(Weights, list):
-            raise TypeError("Weights must be a list of numpy arrays instead got: "+type(Weights).__name__)
+            raise TypeError(
+                "Weights must be a list of numpy arrays instead got: "+type(Weights).__name__)
         if len(Weights) != 2:
-            raise ValueError("Weights must be a list with length 2 instead got: "+ len(Weights))
+            raise ValueError(
+                "Weights must be a list with length 2 instead got: " + len(Weights))
         for i in range(len(Weights)):
             if not isinstance(Weights[i], np.ndarray):
-                raise TypeError("Weights must be a list of only numpy arrays found: "+type(Weights[i]).__name__+" at position: "+str(i))
+                raise TypeError("Weights must be a list of only numpy arrays found: " +
+                                type(Weights[i]).__name__+" at position: "+str(i))
         self.Weights = Weights
-            
-            
-    
-    def buildLayer(self): # Note: Cannot set weights before adding to model
-        #print(np.shape(self.Weights[0]))
-        units = np.shape(self.Weights[0])[1]#Get units for new layer
+
+    def buildLayer(self):  # Note: Cannot set weights before adding to model
+        # print(np.shape(self.Weights[0]))
+        units = np.shape(self.Weights[0])[1]  # Get units for new layer
         return Dense(units, activation=self.activation)
