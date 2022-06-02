8f4879403564070bef2222a7f11b941566a5b430
ornateByte <null@gmail.com>
2022-06-02 02:34:37 -0600

    Fixed Dict

--- src/KerasObese/Dict.py
+++ src/KerasObese/Dict.py
@@ -2,9 +2,9 @@ from keras.activations import *
 
 # dic is formated using the equation y = mx + b where the closest solution is described by (m, b)
 LayerDictionary = {
-    (type(relu), type(relu)): (1, 0),  # perfect transformation
-    (type(tanh), type(tanh)): (1.1251, 0),
-    (type(tanh), type(sigmoid)): (1.1843, 0),
-    (type(sigmoid), type(tanh)): (1.1099, 0),
-    (type(sigmoid), type(sigmoid)): (1.1677, 0)
+    (relu.__name__, relu.__name__): (1, 0),  # perfect transformation
+    (tanh.__name__, tanh.__name__): (1.1251, 0),
+    (tanh.__name__, sigmoid.__name__): (1.1843, 0),
+    (sigmoid.__name__, tanh.__name__): (1.1099, 0),
+    (sigmoid.__name__, sigmoid.__name__): (1.1677, 0)
 }
