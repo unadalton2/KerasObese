aea8bb60fd63edfcbb97299d17d74bc4e364f6da
ornateByte <null@gmail.com>
2022-06-02 15:01:24 -0600

    Added Documentation to Layers

--- src/KerasObese/Layers.py
+++ src/KerasObese/Layers.py
@@ -5,7 +5,37 @@ import numpy as np
 
 
 class Layer:
+    """
+    A class to represent a keras layer
+
+    ...
+
+    Attributes
+    ----------
+    layer : None
+        Copy of keras layer
+    Type : type
+        Type of layer currently stored
+    Weights : list
+        list of layer's weights
+    activation : None
+        Current layer's activation
+
+    Methods
+    -------
+    buildLayer():
+        Retrieves a copy of layer
+    getWeights():
+        Gets the layer's Weights
+    getOutputShape():
+        Gets the expected output shape. Note: Not always possible to get the output shape.
+    """
     def __init__(self, layer):
+        """Creates a Layer of unknown type. Note this layer is not modifiable.
+
+        Args:
+            layer (_type_): Layer to include in final build
+        """
         self.layer = layer
         self.Type = type(layer)
         self.Weights = None
@@ -16,17 +46,71 @@ class Layer:
             self.outputShape = None
 
     def buildLayer(self):
+        """Retrieves a copy of layer
+
+        Returns:
+            _type_: Returns the layer
+        """
         return self.layer
 
-    def getWeights(self):
+    def getWeights(self) -> list:
+        """Gets the layer's Weights
+
+        Returns:
+            list: Gets the layer's Weights
+        """
         return self.Weights
 
-    def getOutputShape(self):
+    def getOutputShape(self) -> tuple:
+        """Gets the expected output shape. Note: Not always possible to get the output shape.
+
+        Returns:
+            tuple: Output Shape
+        """
         return self.outputShape
 
 
 class DenseLayer(Layer):
+    """
+    A class to represent a keras Dense layer
+
+    ...
+
+    Attributes
+    ----------
+    layer : None
+        Copy of keras layer
+    Type : type
+        Type of layer currently stored
+    Weights : list
+        list of layer's weights
+    activation : None
+        Current layer's activation
+
+    Methods
+    -------
+    buildLayer():
+        Creates keras layer from Weights
+    getWeights():
+        Gets the layer's Weights
+    setWeights(Weights):
+        Sets the layers weights
+    getOutputShape():
+        Gets the expected output shape. Note: Not always possible to get the output shape.
+    """
     def __init__(self, layer: Dense, Weights: list = None):
+        """Creates a clone of layer, optionally adds weights to layer.
+
+        Args:
+            layer (Dense): Layer to include in final build
+            Weights (list, optional): Weights to use in the final build instead of default. Defaults to None.
+
+        Raises:
+            TypeError: Raised if layer is not an instance of Dense
+            TypeError: Raised if Weights is not an instance of None or list
+            ValueError: Raised if Weights does not have a length of 2
+            TypeError: Raised Weights is not a list of numpy arrays
+        """
         if not isinstance(layer, Dense):
             raise TypeError(
                 "layer must be a dense layer, instead got "+type(layer).__name__)
@@ -48,8 +132,17 @@ class DenseLayer(Layer):
 
         self.activation = self.layer.activation
 
-    def setWeights(self, Weights):
+    def setWeights(self, Weights: list):
+        """Sets the layers weights
 
+        Args:
+            Weights (list): Weights to use in final build
+
+        Raises:
+            TypeError: Raised if Weights is not an instance of list
+            ValueError: Raised if Weights does not have a length of 2
+            TypeError: Raised Weights is not a list of numpy arrays
+        """
         if not isinstance(Weights, list):
             raise TypeError(
                 "Weights must be a list of numpy arrays instead got: "+type(Weights).__name__)
@@ -62,7 +155,11 @@ class DenseLayer(Layer):
                                 type(Weights[i]).__name__+" at position: "+str(i))
         self.Weights = Weights
 
-    def buildLayer(self):  # Note: Cannot set weights before adding to model
-        # print(np.shape(self.Weights[0]))
+    def buildLayer(self) -> Dense:  # Note: Cannot set weights before adding to model
+        """Creates keras layer from Weights. Note: Does not return layer with Weights only layer of correct size for Weights
+
+        Returns:
+            Dense: Layer capable of receiving Weights once added to model.
+        """
         units = np.shape(self.Weights[0])[1]  # Get units for new layer
         return Dense(units, activation=self.activation)
